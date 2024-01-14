701798f39b40ab9c46ccc489626cc3c859c02f5a
punitkr03 <tagpunitkumar@gmail.com>
2024-01-14 11:46:04 +0530

    "added add file button in frontend"

--- src/App.jsx
+++ src/App.jsx
@@ -16,39 +16,134 @@
  *
  */
 
-import React from 'react'
-import { Container } from 'semantic-ui-react'
-import { Route, Switch, Redirect } from 'react-router-dom'
-import { FileList, Heatmap, FlameGraph, DifferentialFlameGraph, ElidedFlameGraph, Navbar, Error } from './components'
+import React, { useState } from "react";
+import { Container } from "semantic-ui-react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import {
+  FileList,
+  Heatmap,
+  FlameGraph,
+  DifferentialFlameGraph,
+  ElidedFlameGraph,
+  Navbar,
+  Error,
+} from "./components";
+import { Button } from "semantic-ui-react";
+import "../semantic/semantic.less";
 
-import '../semantic/semantic.less'
+const App = () => {
+  const [file, setFile] = useState(null);
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Container>
-      <Switch>
-        <Route exact path="/" component={FileList}/>
-        <Route exact path="/heatmap/:type/:filename" component={Heatmap} />
-        <Route exact path="/flamegraph/:type/:filename/:start/:end" component={FlameGraph} />
-        <Route exact path="/flamegraph/:type/:filename" component={FlameGraph} />
-        <Route exact path="/compare/:compareType/:compareFilename/:compareStart/:compareEnd" component={FileList} />
-        <Route exact path="/compare/:compareType/:compareFilename" component={FileList} />
-        <Route exact path="/compare/:compareType/:compareFilename/:compareStart/:compareEnd/heatmap/:type/:filename" component={Heatmap} />
-        <Route exact path="/compare/:compareType/:compareFilename/heatmap/:type/:filename" component={Heatmap} />
-        <Route exact path="/differential/:type/:filename/:start/:end/compare/:compareType/:compareFilename/:compareStart/:compareEnd" component={DifferentialFlameGraph} />
-        <Route exact path="/differential/:type/:filename/:start/:end/compare/:compareType/:compareFilename" component={DifferentialFlameGraph} />
-        <Route exact path="/differential/:type/:filename/compare/:compareType/:compareFilename/:compareStart/:compareEnd" component={DifferentialFlameGraph} />
-        <Route exact path="/differential/:type/:filename/compare/:compareType/:compareFilename" component={DifferentialFlameGraph} />
-        <Route exact path="/elided/:type/:filename/:start/:end/compare/:compareType/:compareFilename/:compareStart/:compareEnd" component={ElidedFlameGraph} />
-        <Route exact path="/elided/:type/:filename/:start/:end/compare/:compareType/:compareFilename" component={ElidedFlameGraph} />
-        <Route exact path="/elided/:type/:filename/compare/:compareType/:compareFilename/:compareStart/:compareEnd" component={ElidedFlameGraph} />
-        <Route exact path="/elided/:type/:filename/compare/:compareType/:compareFilename" component={ElidedFlameGraph} />
-        <Route exact path="/error/:code" component={Error} />
-        <Redirect to="/error/404" />
-      </Switch>
-    </Container>
-  </div>
-)
+  const handleAddFile = (e) => {
+    e.preventDefault();
+    console.log(file);
+  };
 
-export default App
+  const handleFile = (e) => {
+    e.preventDefault();
+    setFile(e.target.files[0]);
+    console.log(e.target.files[0]);
+  };
+
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Container>
+          <Switch>
+            <Route exact path="/" component={FileList} />
+            <Route exact path="/heatmap/:type/:filename" component={Heatmap} />
+            <Route
+              exact
+              path="/flamegraph/:type/:filename/:start/:end"
+              component={FlameGraph}
+            />
+            <Route
+              exact
+              path="/flamegraph/:type/:filename"
+              component={FlameGraph}
+            />
+            <Route
+              exact
+              path="/compare/:compareType/:compareFilename/:compareStart/:compareEnd"
+              component={FileList}
+            />
+            <Route
+              exact
+              path="/compare/:compareType/:compareFilename"
+              component={FileList}
+            />
+            <Route
+              exact
+              path="/compare/:compareType/:compareFilename/:compareStart/:compareEnd/heatmap/:type/:filename"
+              component={Heatmap}
+            />
+            <Route
+              exact
+              path="/compare/:compareType/:compareFilename/heatmap/:type/:filename"
+              component={Heatmap}
+            />
+            <Route
+              exact
+              path="/differential/:type/:filename/:start/:end/compare/:compareType/:compareFilename/:compareStart/:compareEnd"
+              component={DifferentialFlameGraph}
+            />
+            <Route
+              exact
+              path="/differential/:type/:filename/:start/:end/compare/:compareType/:compareFilename"
+              component={DifferentialFlameGraph}
+            />
+            <Route
+              exact
+              path="/differential/:type/:filename/compare/:compareType/:compareFilename/:compareStart/:compareEnd"
+              component={DifferentialFlameGraph}
+            />
+            <Route
+              exact
+              path="/differential/:type/:filename/compare/:compareType/:compareFilename"
+              component={DifferentialFlameGraph}
+            />
+            <Route
+              exact
+              path="/elided/:type/:filename/:start/:end/compare/:compareType/:compareFilename/:compareStart/:compareEnd"
+              component={ElidedFlameGraph}
+            />
+            <Route
+              exact
+              path="/elided/:type/:filename/:start/:end/compare/:compareType/:compareFilename"
+              component={ElidedFlameGraph}
+            />
+            <Route
+              exact
+              path="/elided/:type/:filename/compare/:compareType/:compareFilename/:compareStart/:compareEnd"
+              component={ElidedFlameGraph}
+            />
+            <Route
+              exact
+              path="/elided/:type/:filename/compare/:compareType/:compareFilename"
+              component={ElidedFlameGraph}
+            />
+            <Route exact path="/error/:code" component={Error} />
+            <Redirect to="/error/404" />
+          </Switch>
+        </Container>
+      </div>
+      <div
+        style={{
+          maxWidth: "screen",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <form>
+          <input type="file" name="file" onChange={handleFile} />
+          <Button color="teal" type="submit" onClick={handleAddFile}>
+            Add File
+          </Button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default App;
