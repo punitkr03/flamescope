6e87d9582900920ec635d5039f33b9ae4b64798d
punitkr03 <tagpunitkumar@gmail.com>
2024-01-14 10:07:31 +0530

    Added setup bash file

---
+++ setup.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/Netflix/flamescope
+cd flamescope
+pip install -r requirements.txt
+brew install node
+npm install react-scripts@latest
+npm install
+npm run webpack
+python3 run.py
